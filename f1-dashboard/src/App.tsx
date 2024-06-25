3d7499aff94597aacbc3f917e47afc359903a155
michaell4438 <michael@lachut.com>
2024-06-24 20:10:51 -0400

    UI Changes

--- f1-dashboard/src/App.tsx
+++ f1-dashboard/src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Container, Row, Col } from 'react-grid-system';
+
+function Dashboard() {
+    return (
+        <>
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <p className={"miscIndicator"}>1:00.00</p>
+                    </Col>
+                    <Col>
+                        <Row>
+                            <p className={"miscIndicator"}>150 km/h</p>
+                        </Row>
+                        <Row>
+                            <p className={"gearIndicator"}>1</p>
+                            <div className={"recommendedGearOuter"}>
+                                <p className={"recommendedGear"}>2</p>
+                            </div>
+                        </Row>
+                    </Col>
+                    <Col>
+                        <Row>
+                            <p className={"miscIndicator smallFont"}>S1: 0.00</p>
+                        </Row>
+                        <Row>
+                            <p className={"miscIndicator smallFont"}>S2: 0.00</p>
+                        </Row>
+                        <Row>
+                            <p className={"miscIndicator smallFont"}>S3: 0.00</p>
+                        </Row>
+                    </Col>
+                </Row >
+            </Container >
+        </>
+    )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [connected, setConnected] = useState(false)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Dashboard />
+        </>
+    )
 }
 
 export default App
---
+++ f1-dashboard/src/network.tsx
@@ -0,0 +1,2 @@
+import dgram from 'dgram';
+const s = dgram.createSocket('udp4');
